5b5ab8cf0eb94edc63f043d0d3f29544d7c640ac
Deborah M Ingabire <ingabineza@gmail.com>
2021-02-23 14:05:41 +0200

    appointments

---
+++ Js/script.js
@@ -0,0 +1,6 @@
+function output(){
+    var a=document.getElementById("fname").value;
+    var b=document.getElementById("phone").value;
+    var c=document.getElementById("message").value;
+    prompt("Hi " + a + ","+" your appointment have been made. Thank you for reaching out to us.");
+    }
